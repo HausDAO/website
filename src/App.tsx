3a795d9161be6e3bb29725467ac506b9191c2df6
Rowdy <mroddy5280@gmail.com>
2023-02-20 13:37:12 -0700

    Added dropshadow to button. Need to think about how to make it a border

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import Header from 'components/Header';
 import Navigation from 'components/Navigation';
 import { Button } from 'components/Button';
 import Footer from 'components/Footer';
+import Card from 'components/Card';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Header />
-      <Button onClick={() => alert('Start Daoing it!')}>DAOHaus</Button>
+      <Card />
       <Footer />
     </div>
   );

d2eca2e3d93293d19777e04846d62aea41da4a2d
Rowdy <mroddy5280@gmail.com>
2023-02-20 09:11:34 -0700

    Built Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from 'components/Header';
 import { Button } from 'components/Button';
+import { Footer } from 'components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header title="DAOHaus!!!" />
       <Button onClick={() => alert('Start Daoing it!')}>DAOHaus</Button>
+      <Footer />
     </div>
   );
 }

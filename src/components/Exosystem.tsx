91b2e1538a4ae12a5f2a3291e645a83a50ab03f3
Rowdy <mroddy5280@gmail.com>
2023-02-21 12:21:35 -0700

    Created files for all sections and pages

---
+++ src/components/Exosystem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Exosystem = () => {
+  return <div>Exosystem</div>;
+};
+
+export default Exosystem;
---
+++ src/pages/Moloch.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Moloch = () => {
+  return <div>Moloch</div>;
+};
+
+export default Moloch;

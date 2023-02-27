bbbd577207a23eec11a0f5a9412a0ce0e9639654
Rowdy <mroddy5280@gmail.com>
2023-02-26 23:07:20 -0700

    fix: mobile nav menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,60 +1,86 @@
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
-
+import { FaDiscord, FaTwitter, FaBars } from 'react-icons/fa';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { ReactComponent as HausLogo } from 'assets/haus-logo.svg';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="bg-white px-2 py-2.5 dark:bg-white sm:px-4">
+    <nav className="bg-white px-2 py-2.5 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <a href="https://daohaus.club/" className="flex items-center">
           <HausLogo height={40} width="auto" />
         </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-brandRed dark:hover:bg-blue-700 dark:focus:ring-blue-300 md:hidden"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
+        <div className="md:hidden">
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+              <button
+                className="shadow-blackA7 hover:bg-violet3 inline-flex h-[35px] w-[35px] items-center justify-center rounded-md bg-white text-brandRed shadow-[0_2px_10px] outline-none focus:outline-none"
+                aria-label="Menu Button"
+              >
+                <FaBars />
+              </button>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
+                className="data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade min-w-[220px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]"
+                align="end"
+              >
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <Link to="/">Home</Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <Link to="/moloch">V3</Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <a href="https://summon.daohaus.fun/" target="_blank" rel="noreferrer">
+                    Summoner
+                  </a>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <a href="https://admin.daohaus.fun/" target="_blank" rel="noreferrer">
+                    Mission CTRL
+                  </a>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <a href="https://docs.daohaus.club/" target="_blank" rel="noreferrer">
+                    Docs
+                  </a>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 group relative flex h-[25px] select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none data-[disabled]:pointer-events-none">
+                  <a href="https://publichaus.club/" target="_blank" rel="noreferrer">
+                    Governance
+                  </a>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator className="bg-violet6 m-[5px] h-[1px]" />
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
+        </div>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-white p-4 dark:border-gray-700 dark:bg-white md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-white">
+          <ul className="md mt-4 flex flex-col rounded-lg border border-gray-100 bg-white p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium">
             <li>
-              <a
-                href="/"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+              <Link
+                to="/"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="/moloch"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+              <Link
+                to="/moloch"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 V3
-              </a>
+              </Link>
             </li>
             <li>
               <a
                 href="https://summon.daohaus.fun/"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 Summoner
               </a>
@@ -64,7 +90,7 @@ const Navigation = () => {
                 href="https://admin.daohaus.fun/"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 Mission CTRL
               </a>
@@ -74,7 +100,7 @@ const Navigation = () => {
                 href="https://docs.daohaus.club/"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 Docs
               </a>
@@ -84,7 +110,7 @@ const Navigation = () => {
                 href="https://publichaus.club/"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 Governance
               </a>
@@ -94,7 +120,7 @@ const Navigation = () => {
                 href="https://discord.com/channels/709210493549674598/735524730328711188"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 <FaDiscord />
               </a>
@@ -104,7 +130,7 @@ const Navigation = () => {
                 href="https://twitter.com/nowdaoit"
                 target="_blank"
                 rel="noreferrer"
-                className="block rounded py-2 pl-3 pr-4 text-blue-700 hover:bg-gray-100 dark:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800 md:dark:hover:bg-transparent md:dark:hover:text-blue-400"
+                className="block rounded py-2 pl-3 pr-4 text-brandBlue hover:bg-gray-100  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-800"
               >
                 <FaTwitter />
               </a>
